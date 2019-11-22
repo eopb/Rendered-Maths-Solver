12f78893b1230bcdd9b623e06446c6586fe65662
Ethan Brierley <ethanboxx@gmail.com>
2019-11-22 13:42:32 +0000

    Clicking maths

--- content.js
+++ content.js
@@ -1,4 +1,27 @@
+function updateMLinks() {
+    console.log("log");
+    const matches = document.querySelectorAll("script[type='math/tex']");
+    matches.forEach(element => {
+        parent = element.parentElement;
+        if (parent.onclick === null) {
+            parent.onclick = function() {
+                wolfram(element.textContent);
+            };
+        }
+    });
+}
+
+updateMLinks();
+
+setInterval(updateMLinks, 1000);
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     const matches = document.querySelectorAll("script[type='math/tex']");
     sendResponse({ count: matches.length });
 });
+
+function wolfram(query) {
+    window.open(
+        `https://www.wolframalpha.com/input/?i=${encodeURIComponent(query)}`
+    );
+}
