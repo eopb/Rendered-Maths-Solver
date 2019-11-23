1b2dd4c6565977c22e52f27c6e0d518c57989e56
Ethan Brierley <ethanboxx@gmail.com>
2019-11-23 09:53:55 +0000

    fail attempt

--- content.js
+++ content.js
@@ -3,6 +3,30 @@ function updateMLinks() {
     const matches = document.querySelectorAll("script[type='math/tex']");
     matches.forEach(element => {
         parent = element.parentElement;
+        onmousemove = function(e) {
+            mouseLoc = { x: e.clientX, y: e.clientY };
+            var overlayRect = document
+                .getElementById("MathsOverlay")
+                .getClientRects();
+            var pRect = parent.getClientRects();
+            console.log(mouseLoc);
+            console.log(
+                overlayRect.top,
+                overlayRect.right,
+                overlayRect.bottom,
+                overlayRect.left
+            );
+            console.log(pRect.top, pRect.right, pRect.bottom, pRect.left);
+            if (pRect.left <= mouseLoc.x && pRect.right >= mouseLoc.x) {
+                console.log("horizontal in");
+            }
+            console.log(
+                `${pRect.top} <= ${mouseLoc.y} && ${pRect.bottom} >= ${mouseLoc.y}`
+            );
+            if (pRect.top <= mouseLoc.y && pRect.bottom >= mouseLoc.y) {
+                console.log("vertical in");
+            }
+        };
         if (parent.onclick === null) {
             parent.onclick = function() {
                 wolfram(element.textContent);
@@ -14,15 +38,7 @@ function updateMLinks() {
                 const iframe = document.createElement("iframe");
                 iframe.id = "MathsOverlay";
                 iframe.setAttribute("src", walframUrl(element.textContent));
-                iframe.height = "200";
-                iframe.width = "400";
-                iframe.style.border = "3px solid rgb(28,110,164)";
-                iframe.style.borderRadius = "5px";
-                iframe.style.boxShadow = "0px 8px 17px -3px rgba(0,0,0,0.54)";
-                iframe.style.backgroundColor = "#fff";
-                iframe.style.position = "fixed";
-                iframe.style.top = `${window.event.clientY - 5}px`;
-                iframe.style.left = `${window.event.clientX - 5}px`;
+                styleIframe(iframe);
                 iframe.classList.add("hoverP");
                 iframe.onmouseover = function() {};
                 iframe.onmouseout = function() {};
@@ -37,6 +53,18 @@ function updateMLinks() {
             };
         }
     });
+}
+
+function styleIframe(iframe) {
+    iframe.height = "200";
+    iframe.width = "400";
+    iframe.style.border = "3px solid rgb(28,110,164)";
+    iframe.style.borderRadius = "5px";
+    iframe.style.boxShadow = "0px 8px 17px -3px rgba(0,0,0,0.54)";
+    iframe.style.backgroundColor = "#fff";
+    iframe.style.position = "fixed";
+    iframe.style.top = `${window.event.clientY - 5}px`;
+    iframe.style.left = `${window.event.clientX - 5}px`;
 }
 
 function removeMath() {
