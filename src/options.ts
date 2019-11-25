e69ef252532b6e44345229d396f599d2c17f93c5
Ethan Brierley <ethanboxx@gmail.com>
2019-11-25 21:44:29 +0000

    Webpack

---
+++ src/options.ts
@@ -0,0 +1,31 @@
+function save_options() {
+  var clickCause: any = document.getElementById("onclick");
+  var clickCause = clickCause.value;
+  console.log("saving");
+  chrome.storage.sync.set(
+    {
+      clickCause: clickCause
+    },
+    function() {
+      var status = document.getElementById("status");
+      status.textContent = "Options saved.";
+      setTimeout(function() {
+        status.textContent = "";
+      }, 750);
+    }
+  );
+}
+
+function restore_options() {
+  chrome.storage.sync.get(
+    {
+      clickCause: "Overlay Window"
+    },
+    function(items) {
+      var x: any = document.getElementById("onclick");
+      x.value = items.clickCause;
+    }
+  );
+}
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", save_options);
